3e9b83e5ff146c35afb10f67cff05de5e9ad0142
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-04-05 16:46:36 +0900

    useFormState done

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,13 +1,45 @@
 'use server';
 
+import { z } from 'zod';
+import validator from 'validator';
+import { redirect } from 'next/navigation';
+
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, 'ko-KR'),
+    'Wrong phone number',
+  );
+
+// coerce: force the value to be a number
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+interface ActionState {
+  token: boolean;
+}
+
 export default async function smsVerification(
-  prevState: any,
+  prevState: ActionState,
   formData: FormData,
 ) {
-  const data = {
-    phone: formData.get('phone'),
-  };
-
-  console.log('Sending verification code to : ', data.phone);
-  return null;
+  const phone = formData.get('phone');
+  const token = formData.get('token');
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return { token: false, error: result.error.flatten() };
+    } else {
+      // send SMS
+      return { token: true };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return { token: true, error: result.error.flatten() };
+    } else {
+      // verify token and login
+      redirect('/');
+    }
+  }
 }
